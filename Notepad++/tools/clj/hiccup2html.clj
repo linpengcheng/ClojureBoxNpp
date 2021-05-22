3b73e4f9ae31055af91d8b647b553eee68967719
Lin Pengcheng <10554885+linpengcheng@users.noreply.github.com>
2021-05-22 10:10:29 +0800

    Update hiccup2html.clj

--- Notepad++/tools/clj/hiccup2html.clj
+++ Notepad++/tools/clj/hiccup2html.clj
@@ -1,3 +1,6 @@
+; Author: Lin Pengcheng
+; My Blog: [The Grand Unified Programming Theory: The Pure Function Pipeline Data Flow with Principle-based Warehouse/Workshop Model](https://github.com/linpengcheng/PurefunctionPipelineDataflow)
+
 (use 'hiccup.core)
 (->> *command-line-args* 
      first
